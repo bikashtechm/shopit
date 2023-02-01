9945d13964b1907ba2e2e26a4ebef485300664dd
Bikash Shaw <1985.bikash.shaw@gmail.com>
2023-02-01 15:57:39 +0530

    seeder to add all records at one go implemented

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,8 +1,22 @@
-const Product = required("../models/product.js");
+const Product = require("../models/product.js");
 
-exports.getProducts = (req, res, next) => {
+// Create New Product ==>  /api/v1/product/new
+
+exports.newProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    sucess: true,
+    product,
+  });
+};
+
+// Get All Products ==> /\api/v1/products
+exports.getProducts = async (req, res, next) => {
+  const products = await Product.find();
   res.status(200).json({
     success: true,
-    message: "This route will show all products in database",
+    count: products.length,
+    message: products,
   });
 };
