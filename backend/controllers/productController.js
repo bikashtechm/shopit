19bafc5ee6da5c4e5848fc5a37dfaab9874f4e95
Bikash Shaw <1985.bikash.shaw@gmail.com>
2023-02-02 21:43:40 +0530

    updateProduct API created

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -37,3 +37,24 @@ exports.getSingleProduct = async (req, res, next) => {
     product,
   });
 };
+
+// Update Product  --> /api/v1/product/:id
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product Not Found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
