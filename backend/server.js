dbfcfe56a034e51161d44960c9ecc63eefd2d781
Bikash Shaw <1985.bikash.shaw@gmail.com>
2023-02-06 12:47:40 +0530

    revert back

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 const app = require("./app");
-var http = require("http");
 const connectDatabase = require("./config/database");
 
 const dotenv = require("dotenv");
+
+// Handle the uncaught exections
+process.on("uncaughtException", (err) => {
+  console.log(`ERRORS: ${err.stack}`);
+  console.log(`Shutting down due to UnCaught Exception`);
+  process.exit(1);
+});
 
 // Setting up Config file
 dotenv.config({ path: "backend/config/config.env" });
@@ -10,12 +16,17 @@ dotenv.config({ path: "backend/config/config.env" });
 //Connecting to Database
 connectDatabase();
 
-var server = http.createServer(function (req, res) {
-  res.end("test");
+const server = app.listen(process.env.PORT, () => {
+  console.log(
+    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`
+  );
 });
 
-server.on("listening", function () {
-  console.log("ok, server is running");
+// Handle "Unhandled Promise Rejection" error
+process.on("unhandledRejection", (err) => {
+  console.log(`ERRORS: ${err.stack}`);
+  console.log(`Shutting down the server due to unhandled promise rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
-
-server.listen(80);
